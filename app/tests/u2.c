07c629abf25f476e22023e002658ad831609e7ab
William Pradier <william.pradier2@gmail.com>
2022-10-13 12:42:18 +0200

    Finished initial testing feature

--- app/tests/u2.c
+++
@@ -1,9 +0,0 @@
-#include "unit-tests.h"
-
-Test(file2_testing, test1) {
-    cr_assert(1);
-}
-
-Test(file2_testing, test2) {
-    cr_assert(1);
-}
---
+++ app/tests/utils.c
@@ -0,0 +1,26 @@
+#include "unit-tests.h"
+#include "cassebrique.h"
+
+char *teststr;
+
+void endStrTest(void) {
+    free(teststr);
+}
+
+
+Test(utils, strnew_fills_allocated_memory_with_zeros, .fini = endStrTest) {
+    size_t testsize;
+
+    testsize = 5;
+
+    teststr = ft_strnew(testsize);
+    for (size_t i = 0; i < testsize + 1; i++) {
+	cr_assert_eq(teststr[0], '\0');
+    }
+}
+
+Test(utils, strnew_allocates_sizearg_plus_one, .fini = endStrTest) {
+    teststr = ft_strnew(0);
+
+    cr_assert_eq(teststr[0], '\0');
+}

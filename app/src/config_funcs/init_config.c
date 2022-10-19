c6334b093cc024c223a5d7cd224ee7bb6678050e
evauclin <vauclinetienne@gmail.com>
2022-10-19 21:20:50 +0200

    create some file and start basic fonction to read a file

--- app/src/config_funcs/init_config.c
+++ app/src/config_funcs/init_config.c
@@ -1,7 +1,35 @@
-#include "cassebrique.h"
+#include "../../includes/cassebrique.h"
+#include <stdio.h>
 
 /* Si ligne de config non valide (mauvaise valeur, mauvais format de ligne) ou non remplie,
  * mettre une valeur par défaut.
 */
 
 t_config      *init_config(char *path);
+
+
+
+int main() {
+
+
+
+
+    FILE    *textfile;
+    char ch;
+    
+    textfile = fopen("../../cassebrique.conf", "r");
+    if(textfile == NULL)
+        return 1;
+
+    while((ch = fgetc(textfile))!=EOF) {
+        putchar(ch);
+    }
+     
+    fclose(textfile);
+    return 0;
+}
+
+
+
+
+

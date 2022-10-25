3a0118607662d5c44c4d0b5f87fb6713eebf9b63
Manveer Singh <singh.manveer93@icloud.com>
2022-10-25 23:15:37 +0200

    Add printing functions with colors (text only, no variable)

--- app/includes/cassebrique.h
+++ app/includes/cassebrique.h
@@ -19,6 +19,8 @@
 # define RED "\033[0;31m"
 # define GREEN "\033[0;32m"
 # define NC "\033[0m"
+# define YELLOW "\033[0;33m"
+# define CYAN "\033[0;36m"
 
 
 /* POWERUPS */
---
+++ app/src/utils/highlight_msg.c
@@ -0,0 +1,10 @@
+//
+// Created by Manveer Singh on 25/10/2022.
+//
+#include "cassebrique.h"
+
+void highlight_msg(char* string){
+    printf(CYAN);
+    printf("\n%s", string);
+    printf(NC);
+}

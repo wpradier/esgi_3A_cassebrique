6510ede2dc5ce0c98c3c5e8d32a449a5d6fde36c
Manveer Singh <singh.manveer93@icloud.com>
2022-11-17 20:40:03 +0100

    Correct display_map() with new characters

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,8 @@
 
 int	  main(void) {
     char  *test_str; // string for test function
+    // t_map *map; // Map to use
+    // char* mapString;
 
     /* Test Function */
     test_str = ft_strnew(28);
@@ -12,6 +14,21 @@ int	  main(void) {
     strcpy(test_str, "Everything is working fine!");
     success_msg(test_str);
     free(test_str);
+
+    /*map = init_map("maps/test.cassebrique");
+    if(map != NULL){
+        mapString = display_map(map);
+
+        printf("\nTaille de la chaîne %lu", strlen(mapString));
+        printf("\n");
+
+        success_msg(mapString);
+
+        free(mapString);
+        highlight_msg("Freed map string");
+
+        free_map(map);
+    }*/
 
     return 0;
 }
--- app/src/map_funcs/display_map.c
+++ app/src/map_funcs/display_map.c
@@ -4,65 +4,60 @@
 #include "cassebrique.h"
 
 char *display_map(t_map *map){
-    //printf("\nDefault bombs amount: %d", map->players_start_bombs);
-    //printf("\nWidth of map: %d", map->width);
-    //printf("\nHeight of map: %d", map->height);
-    //printf("\nMap:\n"):
-    char* mapString;
+    char* mapString; // String to return
     mapString = malloc(sizeof(char)*map->height*(map->width+1)+1);
 
     for (int i = 0; i < map->height; ++i) {
         for (int j = 0; j < map->width; ++j) {
-            //printf("%s", (char*)map->state[i][j].content);
-            // Creation of sttring to return
-            if(map->state[i][j].type != 'p'){
-                if(i == 0 && j == 0){
-                    strcpy(mapString, &map->state[i][j].type);
-                }else{
-                    strcat(mapString, &map->state[i][j].type);
-                }
-            }else{
-                if(map->state[i][j].content != NULL){
+            switch (map->state[i][j].type) {
+                case 'x':
                     if(i == 0 && j == 0){
-                        strcpy(mapString, map->state[i][j].content); // todo content.name
+                        strcpy(mapString, "█");
                     }else{
-                        strcat(mapString, map->state[i][j].content); // todo content.name
+                        strcat(mapString, "█");
                     }
-                }else{
+                    break;
+                case 'm':
+                    if(i == 0 && j == 0){
+                        strcpy(mapString, "▒");
+                    }else{
+                        strcat(mapString, "▒");
+                    }
+                    break;
+                case ' ':
+                    if(i == 0 && j == 0){
+                        strcpy(mapString, "-");
+                    }else{
+                        strcat(mapString, "-");
+                    }
+                    break;
+                case 'p':
+                    // We check the content to show player name, else we add 'p'
+                    if(map->state[i][j].content != NULL){
+                        if(i == 0 && j == 0){
+                            strcpy(mapString, map->state[i][j].content); // todo content.name
+                        }else{
+                            strcat(mapString, map->state[i][j].content); // todo content.name
+                        }
+                    }else{
+                        if(i == 0 && j == 0){
+                            strcpy(mapString, &map->state[i][j].type);
+                        }else{
+                            strcat(mapString, &map->state[i][j].type);
+                        }
+                    }
+                    break;
+                default:
                     if(i == 0 && j == 0){
                         strcpy(mapString, &map->state[i][j].type);
                     }else{
                         strcat(mapString, &map->state[i][j].type);
                     }
-                }
+                    break;
             }
-
         }
-        //printf("\n");
+        // For each new line
         strcat(mapString, "\n");
-    }
-    //success_msg("Map created as string");
-
-    // Print map before returning
-    printf("\n");
-    for (unsigned long i = 0; i < strlen(mapString) ; i++) {
-        switch (mapString[i]) {
-            case 'x':
-                printf("█");
-                break;
-            case 'm':
-                printf("▒");
-                break;
-            case ' ':
-                printf("-");
-                break;
-            case '\n':
-                printf("\n");
-                break;
-            default:
-                printf("%c", mapString[i]);
-                break;
-        }
     }
 
     return mapString;
